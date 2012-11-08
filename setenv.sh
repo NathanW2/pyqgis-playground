6f6e92887ea3201721a03835cb653bd645b9686e
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-11-09 00:07:55 +1000

    Update README

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
-#!/bin/sh/
+#!/bin/sh
 # Change thhese to point to your QGIS install
 export LD_LIBRARY_PATH="/home/woo/dev/qgis/build/output/lib/"
 export PYTHONPATH="/home/woo/dev/qgis/build/output/python/"
