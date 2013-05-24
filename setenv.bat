535c08d639bace95d348d772d1caa45087fe6ede
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-05-24 22:45:17 +1000

    Update bootstrap code

--- setenv.bat
+++ setenv.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
-set PYTHONPATH=C:\OSGeo4W\apps\qgis-dev\python
-Set PATH=C:\OSGeo4W\apps\qgis-dev\bin;%PATH%
-set QGIS_PREFIX_PATH=C:\OSGeo4W\apps\qgis-dev\
+set OSGEO4W_ROOT=C:\OSGeo4W
+set PATH=%OSGEO4W_ROOT%\bin;%OSGEO4W_ROOT%\apps\qgis-dev\bin;%PATH%
+SET PYTHONHOME=%OSGEO4W_ROOT%\apps\Python27
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-dev\python
+set QGIS_PREFIX_PATH=%OSGEO4W_ROOT%\apps\qgis-dev\
+set QGIS_DEBUG=0
+REM set QGIS_LOG_FILE=%~dp0\qgis.log
+REM set QGIS_DEBUG_FILE=%~dp0\qgis-debug.log
